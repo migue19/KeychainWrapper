218524fad4ac57f2a8799e35e6c3fd54d7dc5122
Miguel Mexicano Herrera <miguelmexicano18@gmail.com>
2020-10-09 11:03:23 -0500

    se cambia a metodos publicos

--- KeychainWrapper/Main/KeychainWrapper.swift
+++ KeychainWrapper/Main/KeychainWrapper.swift
@@ -7,28 +7,28 @@
 
 import Foundation
 internal class KeychainWrapper {
-    internal static func get(account: String) throws -> Data? {
+    public static func get(account: String) throws -> Data? {
         if try KeychainOperations.exist(account: account) {
             return try KeychainOperations.retreive(account: account)
         } else {
             throw Errors.operationError
         }
     }
-    internal static func set(value: Data, account: String) throws {
+    public static func set(value: Data, account: String) throws {
         if try KeychainOperations.exist(account: account) {
             try KeychainOperations.update(value: value, account: account)
         } else {
             try KeychainOperations.add(value: value, account: account)
         }
     }
-    internal static func delete(account: String) throws {
+    public static func delete(account: String) throws {
         if try KeychainOperations.exist(account: account) {
             return try KeychainOperations.delete(account: account)
         } else {
             throw Errors.operationError
         }
     }
-    internal static func deleteAll() throws {
+    public static func deleteAll() throws {
         try KeychainOperations.deleteAll()
     }
 }
